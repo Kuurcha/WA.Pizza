979314f74058a208fd29acfecc4ae3a2b5d4ba76
Kuurcha <yhyr@mail.ru>
2022-07-04 12:48:48 +0300

    Revert "- Program.cs refactoring"
    This reverts commit 0b22af60c53ddc77a3e03063fe6ffbf09bd0dc95.

--- WA.PIzza.Web/Program.cs
+++ WA.PIzza.Web/Program.cs
@@ -9,19 +9,68 @@ using Wa.Pizza.Infrasctructure.Services;
 using Wa.Pizza.Infrasctructure.Services.Interfaces;
 using Wa.Pizza.Infrasctructure.Validators;
 using WA.PIzza.Web.Extensions;
-using WA.PIzza.Web.Configuration;
+
 AppDomain.CurrentDomain.SetData("DataDirectory",
     Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
 var builder =
     WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<OrderDataService>();
+builder.Services.AddScoped<BasketDataService>();
+builder.Services.AddScoped<CatalogDataService>();
+
+builder.Services.AddControllers();
+
+var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var fullPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    options.IncludeXmlComments(fullPath);
+});
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+Log.Logger = logger;
+
+Log.Information("Application is starting up...");
+builder.Host.UseSerilog(logger);
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddSeq();
+});
+
+builder.Services.AddControllers().AddFluentValidation(options =>
+{
+    options.AutomaticValidationEnabled = true;
+    options.RegisterValidatorsFromAssemblyContaining<BasketItemValidator>();
+});
 
 
-DBWorkConfiguration.configureDBWork(builder);
-SwaggerConfiguration.configureSwagger(builder);
-CustomLoggerConfiguration.configureLogger(builder);
-DBWorkConfiguration.configureDBWork(builder);
 var app = builder.Build();
+
+
 
 
 // Configure the HTTP request pipeline.
